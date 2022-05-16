22104440a860e26540bceaaffc5ea4405024c14a
Annetkhavere <anneywan5@gmail.com>
2022-05-16 23:06:54 +0300

    add the test file for testing user credentials

---
+++ tests/test_users.py
@@ -0,0 +1,35 @@
+import unittest
+from app.models import User
+from app import db
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username = "annet", email ="annet@gmail.com", bio = "C'est la vie", profile_pic_path = "image_url", password = 'annet')
+        db.session.add(self.new_user)
+        db.session.commit()
+
+    def tearDown(self):
+        User.query.delete()
+        db.session.commit()
+ 
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('annet'))
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertTrue(len(User.query.all())>0)
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_user.username, 'annet')
+        self.assertEquals(self.new_user.email, 'annet@gmail.com')
+        self.assertEquals(self.new_user.bio, 'C\'est la vie')
+        self.assertEquals(self.new_user.profile_pic_path, 'image_url')
+        self.assertTrue(self.new_user.verify_password('annet'))
+
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password 
